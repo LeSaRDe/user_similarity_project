ee4a870ea30f15e0ce0b9a280d477e28235dbe4f
LeSaRDe <fcmeng@udel.edu>
2018-07-28 23:03:20 -0400

    treestr_to_graph
    treestr_to_graph tested fine

--- src/usersimproj/pytest.py
+++ src/usersimproj/pytest.py
@@ -4,12 +4,16 @@ from nltk.tokenize import sent_tokenize
 from nltk.parse import corenlp
 import gensim
 import socket
+import networkx as nx
+import matplotlib.pyplot as plt
 
 #(ROOT (S (NP (NP (NNP Align#[00464321v]) (, ,) (NNP Disambiguate#[00957178v]) (, ,) ) (CC and) (NP (NP (VB Walk#[01904930v])) (PRN (-LRB- -LRB-) (NP (NN ADW)) (-RRB- -RRB-)))) (VP (VBZ is) (NP (NP (DT a) (JJ WordNet-based) (NN approach#[00941140n])) (PP (IN for) (S (VP (VBG measuring#[00647094v]) (NP (NP (JJ semantic#[02842042a]) (NN similarity#[04743605n])) (PP (IN of) (NP (NP (JJ arbitrary#[00718924a]) (NNS pairs#[13743605n])) (PP (IN of) (NP (JJ lexical#[02886629a]) (NNS items#[03588414n]))) (, ,) (PP (IN from) (NP (NN word#[06286395n]) (NNS senses#[03990834n])))))) (PP (TO to) (NP (JJ full#[01083157a]) (NNS texts#[06387980n, 06388579n])))))))) (. .)))
 
 con_sent_tree_str = '(ROOT (S (NP (NP L:Align#00464321v L:Disambiguate#00957178v) L:Walk#01904930v) (NP L:approach#00941140n (S (VP L:measuring#00647094v (NP (NP L:semantic#02842042a L:similarity#04743605n) (NP (NP L:arbitrary#00718924a L:pairs#13743605n) (NP L:lexical#02886629a L:items#03588414n) (NP L:word#06286395n L:senses#03990834n))) (NP L:full#01083157a L:texts#06387980n+06388579n))))))'
 
 sent = 'Align, Disambiguate, and Walk (ADW) is a WordNet-based approach for measuring semantic similarity of arbitrary pairs of lexical items, from word senses to full texts.'
+
+NODE_ID_COUNTER = 0
 
 #con_parser = corenlp.CoreNLPParser(url='http://localhost:9000')
 
@@ -21,7 +25,7 @@ def treestr_to_graph(treestr):
     ret_graph = nx.Graph()
     tree = Tree.fromstring(treestr)
     #checkTree(tree, '0')
-    NODE_ID_COUNTER = 0
+    global NODE_ID_COUNTER
     identifyNodes(tree, 's1:')
     tree.set_label('s1:' + tree.label() + ':' + str(NODE_ID_COUNTER))
     tree_prod = tree.productions()
@@ -86,8 +90,11 @@ def main():
     #t_production = con_sent_tree.productions()
     #print con_sent_tree
     #print t_production
-    #treestr_to_graph(con_sent_tree)
+    sent_tree = treestr_to_graph(con_sent_tree_str)
+    plt.subplot(111)
+    nx.draw(sent_tree, with_labels=True, font_weight='bold')
+    plt.show()
 
-    send_wordsim_request('oo', ['06387980n', '06388579n'], ['03588414n'])
+    #send_wordsim_request('oo', ['06387980n', '06388579n'], ['03588414n'])
 
 main()
